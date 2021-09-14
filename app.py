a77ffc02485b18bc6a6847801a64a92b978649c5
Hidnish <hideonishimura@live.it>
2021-09-14 10:57:53 +0000

    Add register fucntionality

--- app.py
+++ app.py
@@ -27,6 +27,24 @@ def get_movies():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        #return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
