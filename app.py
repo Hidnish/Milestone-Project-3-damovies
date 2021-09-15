0be423309f3b215ff7e16e8f650f0c48067e3fdc
Hidnish <hideonishimura@live.it>
2021-09-15 14:13:56 +0000

    Create add_movie template

--- app.py
+++ app.py
@@ -97,6 +97,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_movie")
+def add_movie():
+    return render_template("add_movie.html")
+
+    
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
