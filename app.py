33234093e66d345292465fdfa5fc49d33763f28c
Hidnish <hideonishimura@live.it>
2021-09-17 23:18:19 +0000

    Create genres page

--- app.py
+++ app.py
@@ -143,6 +143,12 @@ def delete(movie_id):
     return redirect(url_for("get_movies"))
 
 
+@app.route("/get_genres")
+def get_genres():
+    genres = list(mongo.db.genres.find().sort("category_name", 1))
+    return render_template("genres.html", genres=genres)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
